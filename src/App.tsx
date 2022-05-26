0c9163d86ddc80898c9ceec7e2ce798d4390bea8
Premkumarv08 <81767973+Premkumarv08@users.noreply.github.com>
2022-05-26 16:36:11 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+import Layout from "./components/Layout";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Layout />
+    </div>
+  );
+}
+
+//collapseable side bar with a button in another component
+// content to link with side bar
